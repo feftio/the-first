55880abe4a3988727cc7f22a94451aadd468f3b7
Lik Eduard <feft99@gmail.com>
2022-06-16 21:14:32 +0600

    start HomePage

--- src/components/pages/home/index.jsx
+++ src/components/pages/home/index.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from "react";
+import TextField from "../../fields/text-field";
+import css from "./HomePage.module.scss";
 
 export default function HomePage() {
-  return (
-    <div>HomePage</div>
-  )
+    return (
+        <div className={css["container"]}>
+            <div className={css["box"]}>
+                <h1 className="h1" style={{ paddingBottom: "20px" }}>
+                    First ONE
+                </h1>
+                <TextField name="Your name:" fullWidth />
+            </div>
+        </div>
+    );
 }
