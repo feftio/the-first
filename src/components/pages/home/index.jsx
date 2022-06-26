0c485224fddf6a419c29f502d7ee310f5a27a362
Eduard <feft99@gmail.com>
2022-06-26 17:03:46 +0600

    create start and create server buttons for home page

--- src/components/pages/home/index.jsx
+++ src/components/pages/home/index.jsx
@@ -1,15 +1,18 @@
 import React from "react";
-import TextField from "../../fields/text-field";
+import TextField from "src/components/fields/text-field";
 import css from "./HomePage.module.scss";
 
 export default function HomePage() {
     return (
         <div className={css["container"]}>
             <div className={css["box"]}>
-                <h1 className="h1" style={{ paddingBottom: "20px" }}>
+                <h1 className="h1" style={{ paddingBottom: "40px" }}>
                     First ONE
                 </h1>
                 <TextField name="Your name:" fullWidth />
+                <div className={css["empty"]}> </div>
+                <button className={css["start-button"]}>Start</button>
+                <button className={css["create-server-button"]}>Create Server</button>
             </div>
         </div>
     );
