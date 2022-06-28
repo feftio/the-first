38252dfd23a0a7f8efc864fb53fb955edaf13cbb
Eduard <feft99@gmail.com>
2022-06-28 23:58:57 +0600

    change home page box sizes

--- src/components/pages/home/index.jsx
+++ src/components/pages/home/index.jsx
@@ -6,13 +6,18 @@ export default function HomePage() {
     return (
         <div className={css["container"]}>
             <div className={css["box"]}>
-                <h1 className="h1" style={{ paddingBottom: "40px" }}>
+                <h1
+                    className="h1"
+                    style={{ textAlign: "center", paddingBottom: "40px" }}
+                >
                     First ONE
                 </h1>
                 <TextField name="Your name:" fullWidth />
                 <div className={css["empty"]}> </div>
                 <button className={css["start-button"]}>Start</button>
-                <button className={css["create-server-button"]}>Create Server</button>
+                <button className={css["create-server-button"]}>
+                    Create Server
+                </button>
             </div>
         </div>
     );
